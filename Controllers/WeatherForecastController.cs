96b33b2348c56ea6f567740d85c43416f72bf936
Avilio <Avilio@DESKTOP-RC7JQBO>
2022-01-25 20:54:17 -0600

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WSVenta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lst = new List<WeatherForecast>();
+            lst.Add(new WeatherForecast() { Id = 1, Nombre = "Avilio" });
+            lst.Add(new WeatherForecast() { Id = 2, Nombre = "Fernando" });
+            return lst;
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,8 @@
+namespace WSVenta
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
