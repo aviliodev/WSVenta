1dc8c6a0e0b10beecba6db7baa8d9db2f4e64873
aviliodev <quidralion@gmail.com>
2022-01-25 23:01:03 -0600

    4. Obtención de datos con Entity Framework

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet] /*protocolo a usar*/
+        public IActionResult Get()
+        {
+            using(VentaRealContext db = new VentaRealContext())
+            {
+                var lst = db.Clientes.ToList();
+                return Ok(lst);
+            }
+            
+        }
+    }
+}
---
+++ Models/Ventum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WSVenta.Models
+{
+    public partial class Ventum
+    {
+        public Ventum()
+        {
+            Conceptos = new HashSet<Concepto>();
+        }
+
+        public int? IdCliente { get; set; }
+        public long Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal? Total { get; set; }
+
+        public virtual Cliente? IdClienteNavigation { get; set; }
+        public virtual ICollection<Concepto> Conceptos { get; set; }
+    }
+}
