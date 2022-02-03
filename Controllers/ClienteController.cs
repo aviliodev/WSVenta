0d1572874a2e4ad21860e57c49b1a09577c9a02c
aviliodev <quidralion@gmail.com>
2022-02-02 23:03:58 -0600

    6 Programando un sistema de ventas real | Instalación de Angular

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WSVenta.Models;
+using WSVenta.Models.Response;
+using WSVenta.Models.Request;
 
 namespace WSVenta.Controllers
 {
@@ -8,15 +10,103 @@ namespace WSVenta.Controllers
     [ApiController]
     public class ClienteController : ControllerBase
     {
-        [HttpGet] /*protocolo a usar*/
+        [HttpGet] /*protocolo a usar, para especificar que entraremos por Get al método IActionResult*/
         public IActionResult Get()
         {
-            using(VentaRealContext db = new VentaRealContext())
+            Respuesta oRespuesta = new Respuesta();
+
+            try
             {
-                var lst = db.Clientes.ToList();
-                return Ok(lst);
+                using(VentaRealContext db = new VentaRealContext()) /*conección a la base de datos con entity framework*/
+                {
+                    var lst = db.Clientes.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Mensaje = "Exito";
+                    oRespuesta.Data = lst;
+                    //return Ok(lst); /*el método Ok transform la lista en un json*/
+                }
             }
-            
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta); /*el método Ok transform la oRespuesta en un json*/
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClientRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext()) 
+                {
+                    oRespuesta.Exito = 1;
+                    Cliente oCliente = new Cliente();
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Clientes.Add(oCliente);
+                    db.SaveChanges();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Edit(ClientRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using(VentaRealContext db = new VentaRealContext())
+                {
+
+                    Cliente oCliente = db.Clientes.Find(oModel.Id);
+                    oCliente.Nombre = oModel.Nombre;
+                    
+                    db.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+
+                    oRespuesta.Exito= 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }  
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+
+                    Cliente oCliente = db.Clientes.Find(Id);
+
+                    db.Remove(oCliente);
+                    db.SaveChanges();
+
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
         }
     }
 }
---
+++ Models/Response/Respuesta.cs
@@ -0,0 +1,15 @@
+﻿namespace WSVenta.Models.Response
+{
+    public class Respuesta
+    {
+        public int Exito { get; set; }
+        public string Mensaje { get; set; }
+
+        public object Data { get; set; }
+
+        public Respuesta() 
+        {
+            Exito = 0;
+        }
+    }
+}
